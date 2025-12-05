d04be8a7210e9bdc2e21d7227f644c159d74fcd9
Jsingh651 <jangsing02@gmail.com>
2025-12-05 18:29:31 +0000

    improve login error handling

---
+++ src/main/java/com/cloudgate/CloudGateApplication.java
@@ -0,0 +1,7 @@
+package com.cloudgate;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class CloudGateApplication {
+    public static void main(String[] args) { SpringApplication.run(CloudGateApplication.class, args); }
+}
