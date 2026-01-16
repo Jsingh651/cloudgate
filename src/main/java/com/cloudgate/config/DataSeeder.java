8499cb51686b0292cdc6a2c0ac9c26b49f357fc0
Jsingh651 <jangsing02@gmail.com>
2026-01-16 09:09:41 +0000

    seed demo admin user on startup

---
+++ src/main/java/com/cloudgate/config/DataSeeder.java
@@ -0,0 +1,10 @@
+package com.cloudgate.config;
+import com.cloudgate.model.Notification;
+import com.cloudgate.model.User;
+import com.cloudgate.repository.NotificationRepository;
+import com.cloudgate.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class DataSeeder {
