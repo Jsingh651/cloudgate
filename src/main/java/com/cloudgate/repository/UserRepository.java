c43fc2d91ac3141ac78b11aeb96226cb91cb2861
Jsingh651 <jangsing02@gmail.com>
2026-03-17 19:02:32 +0000

    remove unused import

---
+++ src/main/java/com/cloudgate/repository/UserRepository.java
@@ -0,0 +1,6 @@
+package com.cloudgate.repository;
+import com.cloudgate.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+public interface UserRepository extends JpaRepository<User, Long> { Optional<User> findByEmail(String email); }
+
