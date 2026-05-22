f39429971ae0f266a8b92dbeb09d3187bb303d05
Jsingh651 <jangsing02@gmail.com>
2026-05-22 11:59:13 +0000

    minor cleanup

---
+++ src/main/java/com/cloudgate/service/UserService.java
@@ -0,0 +1,8 @@
+package com.cloudgate.service;
+import com.cloudgate.dto.UserDto;
+import com.cloudgate.model.User;
+import com.cloudgate.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+@Service
+public class UserService {
